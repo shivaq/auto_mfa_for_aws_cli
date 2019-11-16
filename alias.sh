df27911a3c53db3cb6f3cb5b575d8270f4b2de75
Yasuaki Shibata <shivaq777@gmail.com>
2019-11-16 21:46:10 +0900

    delete comments

--- alias.sh
+++ alias.sh
@@ -3,16 +3,5 @@
 # Usage
 #  Add source ./<path of aliase.sh> to your ~/.bashrc
 
-setToken(){
-    ~/mfa.sh $1 $2
-    if [ $? -ne 0 ]; then
-        echo "MFA のトークンはセットできませんでした。"
-    else
-        source ~/.token_file
-    fi
-}
-
-alias mfa=setToken
-
-# Make serverless framework load ~/.aws/config
-export AWS_SDK_LOAD_CONFIG=true
+# alias mfa=setToken
+alias mfa=~/auto_mfa_for_aws_cli/mfa.sh $1 $2
